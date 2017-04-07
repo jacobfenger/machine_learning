b8d27708beeb4175de30b4db8981e1abe3660429
Jacob Fenger <jakefenger@gmail.com>
2017-04-06 22:21:31 -0700

    Added some comments

--- linear_regression/regress.py
+++ linear_regression/regress.py
@@ -18,11 +18,13 @@ def load_data(file):
         # convert the line to numbers and not strings
         # NOTE: this line is only valid in Python 2.x
         line = map(float, line)
-
+        
+        # Fill the X matrix and append 1 as the dummy variable to the first column
+        # Removing the use of the dummy variable results in a higher SSE 
         X.append([1, line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12]])
         
         Y.append(line[13])
-    
+
     # Convert X and Y to numpy matrices
     X = np.matrix(X)
     Y = np.transpose(np.matrix(Y)) # Make Y a column vector
@@ -40,7 +42,7 @@ def compute_optimal_weight_vector(X, Y):
     # Compute optimal weight vector
     w = np.dot(w_1, w_2)
 
-    print("Optimal weight vector: ", w)
+    #print("Optimal weight vector: ", w)
 
     return w
 
@@ -52,12 +54,14 @@ def compute_sum_of_squared_error(w, X, Y):
     return float(SSE[0])
 
 def main(args):
-
+    
+    # Ensure enough arguments are present
     if len(args) != 3:
         print "Incorrect number of command lind arguments provided."
         print "Usage: <train file name' 'testing file name'"
         return
     
+    # Save file names
     training_file = args[1]
     test_file = args[2]
 
@@ -71,7 +75,7 @@ def main(args):
     SSE_train = compute_sum_of_squared_error(w, train_X, train_Y)
     SSE_test = compute_sum_of_squared_error(w, test_X, test_Y)    
 
-    print "\nSSE from the training data: ", SSE_train
+    print "SSE from the training data: ", SSE_train
     print "SSE from the test data: ", SSE_test
 
 if __name__ == "__main__":
